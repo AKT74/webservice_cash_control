c5644e7e023600830300fdeb2f287ba3316c7256
AKT74 <nayakat74@gmail.com>
2024-07-01 13:45:30 +0700

    push 7th

--- server/controller/wallet.js
+++ server/controller/wallet.js
@@ -31,7 +31,7 @@ router.get("/wallet", async (req, res) => {
       const userId = req.params.id;
   
       const { data, error } = await supabase
-        .from("Wallet, Category(*)")
+        .from("Wallet, Category(wallet_name)")
         .select("*")
         .eq("user_id", userId);
   
